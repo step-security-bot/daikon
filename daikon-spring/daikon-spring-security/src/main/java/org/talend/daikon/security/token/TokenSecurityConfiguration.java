a1e3dbf4ce8816dfe87b720cfe3cf02798b89649
Fabien Abballe <fabballe@talend.com>
2019-07-29 13:02:59 +0200

    feat(TDP-7278): add ConditionalOnBean condition on TokenSecurityConfiguration (#528)

--- daikon-spring/daikon-spring-security/src/main/java/org/talend/daikon/security/token/TokenSecurityConfiguration.java
+++ daikon-spring/daikon-spring-security/src/main/java/org/talend/daikon/security/token/TokenSecurityConfiguration.java
@@ -2,6 +2,10 @@ package org.talend.daikon.security.token;
 
 import static org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.toAnyEndpoint;
 
+import java.util.List;
+
+import javax.servlet.Filter;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,7 @@ import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointPr
 import org.springframework.boot.actuate.endpoint.EndpointId;
 import org.springframework.boot.actuate.endpoint.web.PathMappedEndpoint;
 import org.springframework.boot.actuate.endpoint.web.PathMappedEndpoints;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,9 +26,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import javax.servlet.Filter;
-import java.util.List;
 
 /**
  * A Spring Security configuration class that ensures the Actuator (as well as paths in
@@ -36,6 +38,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @Order(1)
+@ConditionalOnBean(PathMappedEndpoints.class)
 public class TokenSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenSecurityConfiguration.class);
@@ -54,7 +57,7 @@ public class TokenSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final List<TokenProtectedPath> additionalProtectedEndpoints;
 
     public TokenSecurityConfiguration(@Value("${talend.security.token.value:}") String token,
-                                      @Autowired List<TokenProtectedPath> additionalProtectedEndpoints) {
+            @Autowired List<TokenProtectedPath> additionalProtectedEndpoints) {
         this.additionalProtectedEndpoints = additionalProtectedEndpoints;
         additionalProtectedEndpoints.add(() -> "/version");
         final AntPathRequestMatcher[] matchers = additionalProtectedEndpoints.stream() //
