2021ecc5b9c0ece6e51b2cb61cf4d36e0204203c
fhuaulme <fhuaulme@talend.com>
2019-08-07 18:04:23 +0200

    * code format.

--- daikon/src/test/java/org/talend/daikon/security/CryptoHelperTest.java
+++ daikon/src/test/java/org/talend/daikon/security/CryptoHelperTest.java
@@ -1,21 +1,23 @@
 package org.talend.daikon.security;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.talend.daikon.crypto.KeySources;
-
-import static org.junit.Assert.*;
 
 public class CryptoHelperTest {
 
     @Test
     public void testCryptoHelperLegacyPassphrase() {
-        assertEquals("99ZwBDt1L9yMX2ApJx fnv94o99OeHbCGuIHTy22 V9O6cZ2i374fVjdV76VX9g49DG1r3n90hT5c1", CryptoHelper.PASSPHRASE);
+        assertEquals("99ZwBDt1L9yMX2ApJx fnv94o99OeHbCGuIHTy22 V9O6cZ2i374fVjdV76VX9g49DG1r3n90hT5c1",
+                CryptoHelper.PASSPHRASE);
     }
 
     @Test
     public void testCryptoHelperLegacySalt() throws Exception {
-        byte[] expected = new byte[] { (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32, (byte) 0x56, (byte) 0x35, (byte) 0xE3,
-                (byte) 0x03 };
+        byte[] expected = new byte[] { (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32, (byte) 0x56, (byte) 0x35,
+                (byte) 0xE3, (byte) 0x03 };
         assertArrayEquals(expected, KeySources.file("system.encryption.salt").getKey());
     }
 }
