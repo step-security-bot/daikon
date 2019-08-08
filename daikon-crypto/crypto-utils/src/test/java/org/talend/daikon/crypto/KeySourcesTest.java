fe7d6680dbdd85b8f2e8e2adc942993c1a8cebea
Francois Huaulme <fhuaulme@talend.com>
2019-08-08 09:52:02 +0200

    fix(TDKN-256) backport TDKN-256 to 1.7 (#537)
    * feat(TDKN-256): Add file source for keys (#533)
    * Add KeySource implementation that reads key values from file (and external file).
    * Use file key source in CryptoHelper.
    * fix(TDKN-256) Add salt to externalized properties. (#534)

--- daikon-crypto/crypto-utils/src/test/java/org/talend/daikon/crypto/KeySourcesTest.java
+++ daikon-crypto/crypto-utils/src/test/java/org/talend/daikon/crypto/KeySourcesTest.java
@@ -2,6 +2,8 @@ package org.talend.daikon.crypto;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class KeySourcesTest {
@@ -37,13 +39,39 @@ public class KeySourcesTest {
         assertSource(KeySources.systemProperty("missingSystemProperty"));
     }
 
-    @Test()
+    @Test
     public void shouldGenerateFromSystemProperty() throws Exception {
         try {
             System.setProperty("aes.encryption.key", "DataPrepIsSoCool");
             assertSource(KeySources.systemProperty("aes.encryption.key"));
         } finally {
             System.clearProperty("aes.encryption.key");
+        }
+    }
+
+    @Test
+    public void testDefaultFileKeys() throws Exception {
+        final String systemKey = new String(KeySources.file("system.encryption.key").getKey(), EncodingUtils.ENCODING);
+        assertEquals("99ZwBDt1L9yMX2ApJx fnv94o99OeHbCGuIHTy22 V9O6cZ2i374fVjdV76VX9g49DG1r3n90hT5c1", systemKey);
+
+        final String propertiesKey = new String(KeySources.file("properties.encryption.key").getKey(), EncodingUtils.ENCODING);
+        assertEquals("Il faudrait trouver une passphrase plus originale que celle-ci!", propertiesKey);
+    }
+
+    @Test
+    public void testFileKeyOverride() throws Exception {
+        try {
+            final File override = new File(KeySourcesTest.class.getResource("/key_override.dat").toURI());
+            System.setProperty("encryption.keys.file", override.getAbsolutePath());
+
+            final String systemKey = new String(KeySources.file("system.encryption.key").getKey(), EncodingUtils.ENCODING);
+            assertEquals("99ZwBDt1L9yMX2ApJx fnv94o99OeHbCGuIHTy22 V9O6cZ2i374fVjdV76VX9g49DG1r3n90hT5c1", systemKey);
+
+            final String propertiesKey = new String(KeySources.file("properties.encryption.key").getKey(),
+                    EncodingUtils.ENCODING);
+            assertEquals("it's base64 encoded", propertiesKey);
+        } finally {
+            System.setProperty("encryption.keys.file", "");
         }
     }
 
