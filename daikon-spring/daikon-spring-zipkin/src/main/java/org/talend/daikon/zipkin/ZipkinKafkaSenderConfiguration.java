5f90ee84ce5d85ad95c95401800031bc5f3eb0b2
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2020-05-29 14:50:59 +0200

    feat(TDP-8780): Update ZipkinKafkaSenderConfiguration bean definition (#616)
    * Following Spring cloud sleuth documentation, bean should have a specific name in order to override the auto-configuration of Zipkin

--- daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
+++ daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
@@ -1,21 +1,23 @@
 package org.talend.daikon.zipkin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Reporter;
 import zipkin2.reporter.Sender;
 import zipkin2.reporter.kafka.KafkaSender;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Configures the zipkin kafka sender for another kafka broker than the functional broker.
@@ -31,8 +33,12 @@ public class ZipkinKafkaSenderConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(ZipkinKafkaSenderConfiguration.class);
 
-    @Bean
-    @ConditionalOnMissingBean
+    @Bean(ZipkinAutoConfiguration.REPORTER_BEAN_NAME)
+    public Reporter<Span> myReporter(Sender kafkaSender) {
+        return AsyncReporter.create(kafkaSender);
+    }
+
+    @Bean(ZipkinAutoConfiguration.SENDER_BEAN_NAME)
     public Sender kafkaSender(ZipkinKafkaProperties zipkinKafkaProperties, KafkaProperties kafkaProperties) {
         String bootstrapServers = zipkinKafkaProperties.getBootstrapServers();
 
