34416a3b23d4717c1701e3bddbd3716a6399c0c8
Francois Huaulme <fhuaulme@talend.com>
2020-07-28 21:20:35 +0200

    fix(content-service) Fix getUrl/getUri when using custom authentication mode (#637)
    * Fix URL and URI construction when using a non-AWS client.
    * Remove dependency on AWS regions when using Minio.
    
    Co-authored-by: Fabien Abballe <fabballe@talend.com>

--- daikon-spring/daikon-content-service/s3-content-service/src/main/java/org/talend/daikon/content/s3/S3ResourceResolver.java
+++ daikon-spring/daikon-content-service/s3-content-service/src/main/java/org/talend/daikon/content/s3/S3ResourceResolver.java
@@ -2,11 +2,12 @@ package org.talend.daikon.content.s3;
 
 import static org.talend.daikon.content.s3.LocationUtils.toS3Location;
 import static org.talend.daikon.content.s3.LocationUtils.S3PathBuilder.builder;
+import static org.talend.daikon.content.s3.S3ContentServiceConfiguration.EC2_AUTHENTICATION;
 
 import java.io.IOException;
 
-import io.micrometer.core.annotation.Timed;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.WritableResource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.talend.daikon.content.AbstractResourceResolver;
@@ -15,16 +16,21 @@ import org.talend.daikon.content.s3.provider.S3BucketProvider;
 
 import com.amazonaws.services.s3.AmazonS3;
 
+import io.micrometer.core.annotation.Timed;
+
 public class S3ResourceResolver extends AbstractResourceResolver {
 
     private final AmazonS3 amazonS3;
 
     private final S3BucketProvider bucket;
 
-    S3ResourceResolver(ResourcePatternResolver delegate, AmazonS3 amazonS3, S3BucketProvider bucket) {
+    private final Environment environment;
+
+    S3ResourceResolver(ResourcePatternResolver delegate, AmazonS3 amazonS3, S3BucketProvider bucket, Environment environment) {
         super(delegate);
         this.amazonS3 = amazonS3;
         this.bucket = bucket;
+        this.environment = environment;
     }
 
     @Timed
@@ -52,7 +58,18 @@ public class S3ResourceResolver extends AbstractResourceResolver {
                 .append(bucket.getRoot()) //
                 .append(toS3Location(location)) //
                 .build();
-        return super.getResource("s3://" + s3Location);
+
+        final String authentication = environment
+                .getProperty(S3ContentServiceConfiguration.CONTENT_SERVICE_STORE_AUTHENTICATION, EC2_AUTHENTICATION)
+                .toUpperCase();
+        switch (authentication) {
+        case S3ContentServiceConfiguration.MINIO_AUTHENTICATION:
+        case S3ContentServiceConfiguration.CUSTOM_AUTHENTICATION:
+            final String host = environment.getProperty(S3ContentServiceConfiguration.S3_ENDPOINT_URL);
+            return new FixedURLS3Resource(host, s3Location, super.getResource("s3://" + s3Location));
+        default:
+            return super.getResource("s3://" + s3Location);
+        }
     }
 
     @Override
