68ee2122bf6561b5d1f4559cfcd88f39e35cc66c
Francois Huaulme <fhuaulme@talend.com>
2021-06-22 17:23:54 +0200

    chore(versions) Inherits from daikon SB versions (#756)
    * Change parent hierarchy for Spring examples (to ease next releases).

--- daikon-spring/daikon-spring-examples/daikon-spring-example-security/src/main/java/org/talend/demo/actuator/ActuatorApplication.java
+++ daikon-spring/daikon-spring-examples/daikon-spring-example-security/src/main/java/org/talend/demo/actuator/ActuatorApplication.java
@@ -2,12 +2,11 @@ package org.talend.demo.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActuatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
 }
