2f4feb14072b382249e09cc1e582591918ef0ace
C​⁠‌​⁠⁠‌​﻿​⁠‍‌‌​​‍‌yprien Q​⁠‌​⁠⁠‌​﻿​⁠‍‌‌​​‍‌uilici <cquilici@talend.com>
2022-07-28 16:28:15 +0200

    feat(TDP-10831/TQL): Allow conversion of a TQL AST to DSEL (#872)

--- daikon-tql/daikon-tql-dsel/src/main/java/org/talend/tqldsel/DSELConverter.java
+++ daikon-tql/daikon-tql-dsel/src/main/java/org/talend/tqldsel/DSELConverter.java
@@ -10,25 +10,34 @@ import org.talend.tql.parser.Tql;
 
 public class DSELConverter {
 
-    private static DSELVisitor visitor = new DSELVisitor();
+    private static final DSELVisitor visitor = new DSELVisitor();
 
     /**
      * Utility method to convert a TQL query to a DSEL query.
      *
      * @param tqlQuery TQL query as String
-     * @return DSEL ELNode ready to serve for DSEL intepreter
+     * @return DSEL ELNode ready to serve for DSEL interpreter
      */
-    public static ELNode convert(String tqlQuery) throws TqlException {
-        Expression exp = Tql.parse(tqlQuery);
-        ELNode raw = exp.accept(visitor);
+    public static ELNode convert(final String tqlQuery) throws TqlException {
+        final Expression tqlExpression = Tql.parse(tqlQuery);
+        return convert(tqlExpression);
+    }
 
+    /**
+     * Utility method to convert a TQL query to a DSEL query.
+     *
+     * @param tqlQuery TQL query
+     * @return DSEL ELNode ready to serve for DSEL interpreter
+     */
+    public static ELNode convert(final Expression tqlQuery) throws TqlException {
+        final ELNode raw = tqlQuery.accept(visitor);
         return wrapNode(raw);
     }
 
     /**
-     * Wraps an ELNode inside an ELNodeType.ROOT node
-     * (mendatory for interpreting)
-     * 
+     * Wraps an ELNode inside an {@link ELNodeType#ROOT} node
+     * (mandatory for interpreting)
+     *
      * @param node ELNode to be wrapped
      * @return ELNode wrapped
      */
@@ -43,8 +52,8 @@ public class DSELConverter {
 
     /**
      * Utility class to print the ELNode as a tree
-     * 
-     * @param tree
+     *
+     * @param tree ELNode to print
      */
     public static void printELNode(ELNode tree) {
         Logger LOGGER = LoggerFactory.getLogger(DSELVisitor.class);
