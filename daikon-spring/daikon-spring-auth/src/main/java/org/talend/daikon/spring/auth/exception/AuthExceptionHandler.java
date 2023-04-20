961be6c43a95939db628d304932419a8ca70972a
Mark Butko <58919992+mbutko@users.noreply.github.com>
2023-04-20 16:35:19 +0300

    fix(TPSVC-20032): prioritize auth exception handler (#964)

--- daikon-spring/daikon-spring-auth/src/main/java/org/talend/daikon/spring/auth/exception/AuthExceptionHandler.java
+++ daikon-spring/daikon-spring-auth/src/main/java/org/talend/daikon/spring/auth/exception/AuthExceptionHandler.java
@@ -2,6 +2,7 @@ package org.talend.daikon.spring.auth.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Order(-100) // this handler must have higher priority than ones defined in services
 public class AuthExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthExceptionHandler.class);
